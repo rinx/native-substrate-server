2142a5b80b7fb42e96443392597256ffb8a0e99f
Rintaro Okamura <rokamura@yahoo-corp.jp>
2020-01-29 00:25:01 +0900

    :loud_sound: config
    Signed-off-by: Rintaro Okamura <rokamura@yahoo-corp.jp>

--- src/substrate_sample/core.clj
+++ src/substrate_sample/core.clj
@@ -29,9 +29,24 @@
     :routes [{:route "/readiness"
               :body "ok"}]}})
 
+(def timbre-config
+  {:output-fn
+   (fn [{:keys [level] :as data}]
+     (timbre/color-str 
+       (case level
+         :trace :cyan
+         :debug :blue
+         :info :green
+         :warn :yellow
+         :error :red
+         :fatal :red
+         :report :red)
+       (timbre/default-output-fn data)))})
+
 (def shutdown-hook (atom nil))
 
 (defn run [config]
+  (timbre/merge-config! timbre-config)
   (timbre/set-level! :info)
   (let [opts (or config default-opts)
         system (system/system opts)]
